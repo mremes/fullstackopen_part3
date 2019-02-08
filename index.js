57533098506411747afaf4c71c51dd74a7405b02
Matti Remes <remes@iki.fi>
2019-02-08 13:38:53 +0200

    3.10

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.post('/api/persons', (req, res) => {
     res.status(200).end()
 });
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Running server on port ${PORT}`)
 })
